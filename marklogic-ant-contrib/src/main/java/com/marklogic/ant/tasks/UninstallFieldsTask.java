8836eb8673195535847d5e54f03e6a47cba09f27
Gavin Haydon <gphaydon@hotmail.com>
2012-06-07 16:49:48 +0100

    Corrected UninstallFields task

--- marklogic-ant-contrib/src/main/java/com/marklogic/ant/tasks/UninstallFieldsTask.java
+++ marklogic-ant-contrib/src/main/java/com/marklogic/ant/tasks/UninstallFieldsTask.java
@@ -7,12 +7,12 @@ import com.marklogic.ant.annotation.AntTask;
 /**
  * @author Gavin Haydon <gavin.haydon@pressassociation.com>
  */
-@AntTask("uninstall-indices")
+@AntTask("uninstall-fields")
 public class UninstallFieldsTask extends AbstractUninstallTask {
 
 	@Override
 	public void execute() throws BuildException {
 		super.execute();
-		uninstallIndices();
+		uninstallFields();
 	}
 }
